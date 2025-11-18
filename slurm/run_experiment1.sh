d45d0101f8078ee3de9c6ee69edb181b465a5eda
jkfids <jfidelkam@gmail.com>
2025-11-18 21:36:22 +0800

    Update experiment1 slurm script

--- slurm/run_experiment1.sh
+++ slurm/run_experiment1.sh
@@ -2,9 +2,9 @@
 #SBATCH --account=mx95
 #SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=64
-#SBATCH --mem-per-cpu=2000
-#SBATCH --time=0-00:30:00
+#SBATCH --cpus-per-task=48
+#SBATCH --mem-per-cpu=3000
+#SBATCH --time=0-01:00:00
 #SBATCH --output=/home/jkam/mx95_scratch2/jkam/corrqec2_results/logs/experiment1_%j.out
 #SBATCH --error=/home/jkam/mx95_scratch2/jkam/corrqec2_results/logs/experiment1_%j.err
 
