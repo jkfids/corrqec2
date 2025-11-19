f3b39cc9faaf7242cb19a8cf9dcba19af168d0a3
jkfids <jfidelkam@gmail.com>
2025-11-20 00:53:47 +0800

    Rewrite storm model HMM sampling to use Numba instead of Dynamax

--- src/corrqec2/noisemodels/storm_model.py
+++ src/corrqec2/noisemodels/storm_model.py
@@ -1,21 +1,56 @@
 import os
 import numpy as np
+from numba import njit
 import stim
-
-# Set JAX platform before import
-_JAX_DEVICE = os.environ.get("JAX_PLATFORMS", "cpu")
-if "JAX_PLATFORMS" not in os.environ:
-    os.environ["JAX_PLATFORMS"] = _JAX_DEVICE
-
-import jax
-import jax.numpy as jnp
-import jax.random as jr
-from jax import vmap
-from dynamax.hidden_markov_model import CategoricalHMM
 
 from ..experiments.base_experiment import Experiment
 from ..experiments.experiment_utils import combine_split_circuits
 from .base_noise_model import NoiseModel
+
+
+@njit
+def _sample_storm_hmm_batch(
+    n_chains: int,
+    n_rounds: int,
+    initial_probs: np.ndarray,  # shape (2,)
+    transition_matrix: np.ndarray,  # shape (2, 2)
+    emission_probs: np.ndarray,  # shape (2, 4)
+) -> np.ndarray:
+    """_summary_
+
+    Args:
+        n_chains (int): _description_
+        n_rounds (int): _description_
+        initial_probs (np.ndarray): _description_
+
+    Returns:
+        np.ndarray: _description_
+    """
+    samples = np.empty((n_chains, n_rounds), dtype=np.int8)
+
+    for i in range(n_chains):
+        u = np.random.rand()
+        if u < initial_probs[0]:
+            state = 0
+        else:
+            state = 1
+
+        for j in range(n_rounds):
+            v = np.random.rand()
+            cumulative = 0.0
+            for k in range(4):
+                cumulative += emission_probs[state, k]
+                if v < cumulative:
+                    break
+            samples[i, j] = k
+
+            w = np.random.rand()
+            if w < transition_matrix[state, 0]:
+                state = 0
+            else:
+                state = 1
+
+    return samples
 
 
 class StormModel(NoiseModel):
@@ -26,29 +61,23 @@ class StormModel(NoiseModel):
         noisy_qubit_types: str | list[str] = "all",
     ):
         super().__init__(gate_noise, noisy_qubit_types)
-        self.model_params = model_params
         self._no_error_matrix = False
+        self.model_params = model_params
 
-        self.hmm, self.hmm_params = self._init_hmm()
-
-    def _init_hmm(self):
-        a = self.model_params["a"]  # transition prob. from calm state to stormy state
-        b = self.model_params["b"]  # transition prob. from stormy state to calm state
-        emissions = self.model_params["emissions"]
-        pi_a = a / (a + b)  # stationary prob. of being in stormy state
-        pi_b = b / (a + b)  # stationary prob. of being in calm state
-
-        # num_states: Number of hidden states
-        # emission_dim: Dimension of the emission space
-        # num_classes: Size of the discrete emission alphabet
-        hmm = CategoricalHMM(num_states=2, emission_dim=1, num_classes=4)
-        params, _ = hmm.initialize(
-            initial_probs=jnp.array([pi_b, pi_a]),
-            transition_matrix=jnp.array([[1.0 - a, a], [b, 1.0 - b]]),
-            emission_probs=jnp.array(emissions).reshape(2, 1, 4),
-        )
-
-        return hmm, params
+        # Extract model parameters
+        a, b = model_params["a"], model_params["b"]
+        self._a = a
+        self._b = b
+        # Transfer/transition matrix
+        self._T = np.array([[1.0 - a, a], [b, 1.0 - b]])
+        # Emission probabilities
+        self._emissions = np.array(model_params["emissions"])
+        # stationary prob. of being in stormy state
+        self._pi_a = a / (a + b)
+        # stationary prob. of being in calm state
+        self._pi_b = b / (a + b)
+        # Initial probabilities
+        self._initial_probs = np.array([self._pi_b, self._pi_a])
 
     def gen_error_matrix(
         self, experiment: Experiment, n_samples: int = 1
@@ -69,15 +98,17 @@ class StormModel(NoiseModel):
             qubit_types=self.noisy_qubit_types
         )
 
-        hmm = self.hmm
-        params = self.hmm_params
+        samples = _sample_storm_hmm_batch(
+            n_chains=n_qubits * n_samples,
+            n_rounds=n_rounds,
+            initial_probs=self._initial_probs,
+            transition_matrix=self._T,
+            emission_probs=self._emissions,
+        )
 
-        key = jr.PRNGKey(np.random.randint(0, 2**32))
-        keys = jr.split(key, n_qubits * n_samples)
-        _, samples = vmap(lambda k: hmm.sample(params, k, n_rounds))(keys)
-        error_matrix = samples
+        error_matrix = samples.reshape(n_samples, n_qubits, n_rounds)
 
-        return error_matrix.reshape(n_samples, n_qubits, n_rounds)
+        return error_matrix
 
     def gen_marginalized_circuit(self, experiment: Experiment) -> stim.Circuit:
         """_summary_
@@ -99,13 +130,11 @@ class StormModel(NoiseModel):
             split_circuits = experiment.split_circuits
 
         # Calculate marginal error probabilities
-        a = self.model_params["a"]
-        b = self.model_params["b"]
-        emissions = self.model_params["emissions"]
-        calm_fraction = b / (a + b)
-        storm_fraction = a / (a + b)
+        emissions = self._emissions
+        calm_fraction = self._pi_b
+        storm_fraction = self._pi_a
         p_I = calm_fraction * emissions[0][0] + storm_fraction * emissions[1][0]
-        p_D = 1 - p_I  # Marginal independent depolarizing probability
+        p_D = 1.0 - p_I  # Marginal independent depolarizing probability
 
         # Get qubit targets for marginal channel injection
         targets = experiment.get_qubits_by_type(self.noisy_qubit_types)
@@ -130,58 +159,6 @@ class StormModel(NoiseModel):
         )
 
         return new_circuit
-
-    # def gen_marginalized_circuit(self, experiment: Experiment) -> stim.Circuit:
-    #     """_summary_
-
-    #     Args:
-    #         experiment (Experiment): _description_
-
-    #     Raises:
-    #         NotImplementedError: _description_
-
-    #     Returns:
-    #         stim.Circuit: _description_
-    #     """
-
-    #     # Get base circuit
-    #     if self.gate_noise is not None:
-    #         init, init_round, (repeat_count, repeat_block), final = (
-    #             self.gen_noisy_circuit(experiment, split_circuit=True)
-    #         )
-    #     else:
-    #         init, init_round, (repeat_count, repeat_block), final = (
-    #             experiment.split_circuits
-    #         )
-
-    #     # Calculate marginal error probabilities
-    #     a = self.model_params["a"]
-    #     b = self.model_params["b"]
-    #     emissions = self.model_params["emissions"]
-    #     calm_fraction = b / (a + b)
-    #     storm_fraction = a / (a + b)
-    #     p_I = calm_fraction * emissions[0][0] + storm_fraction * emissions[1][0]
-    #     p_D = 1 - p_I  # Marginal independent depolarizing probability
-
-    #     # Get qubit targets for marginal channel injection
-    #     targets = experiment.get_qubits_by_type(self.noisy_qubit_types)
-
-    #     # Append marginalized depolarization to the start of init_round
-    #     init_round_new = stim.Circuit()
-    #     init_round_new.append("DEPOLARIZE1", targets, p_D)
-    #     init_round_new += init_round
-
-    #     # Append marginalized depolarization to the start of repeat_block
-    #     repeat_block_new = stim.Circuit()
-    #     repeat_block_new.append("DEPOLARIZE1", targets, p_D)
-    #     repeat_block_new += repeat_block
-
-    #     # Combine all parts back into a single circuit
-    #     new_circuit = combine_split_circuits(
-    #         [init, init_round_new, (repeat_count, repeat_block_new), final]
-    #     )
-
-    #     return new_circuit
 
     def gen_detector_error_model(
         self, experiment: Experiment
