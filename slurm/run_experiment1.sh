4d8768cb6181e7e4ab15d76f81c6f47756605e8d
jkfids <jfidelkam@gmail.com>
2025-11-19 18:03:12 +0800

    Attempt number 6

--- slurm/run_experiment1.sh
+++ slurm/run_experiment1.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=32
 # SBATCH --mem-per-cpu=8000
-#SBATCH --mem=300G
+#SBATCH --mem=256G
 #SBATCH --time=0-08:00:00
 #SBATCH --output=/home/jkam/mx95_scratch2/jkam/corrqec2_results/logs/experiment1_%j.out
 #SBATCH --error=/home/jkam/mx95_scratch2/jkam/corrqec2_results/logs/experiment1_%j.err
@@ -15,17 +15,16 @@
 # ============================================================================
 
 # Thread limiting for BLAS/OpenMP stuff
-# export OMP_NUM_THREADS=1
-# export MKL_NUM_THREADS=1
-# export OPENBLAS_NUM_THREADS=1
-# export NUMEXPR_NUM_THREADS=1
-# FLEXIBLAS is optional; only matters if you’re actually using it:
-# export FLEXIBLAS_NUM_THREADS=1
+export OMP_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+export OPENBLAS_NUM_THREADS=1
+export NUMEXPR_NUM_THREADS=1
+export FLEXIBLAS_NUM_THREADS=1
 
 # JAX/XLA behaviour
-# export JAX_PLATFORMS=cpu
-# export XLA_FLAGS="--xla_cpu_multi_thread_eigen=false intra_op_parallelism_threads=1"
-# export XLA_PYTHON_CLIENT_PREALLOCATE=false
+export JAX_PLATFORMS=cpu
+export XLA_FLAGS="--xla_cpu_multi_thread_eigen=false intra_op_parallelism_threads=1"
+export XLA_PYTHON_CLIENT_PREALLOCATE=false
 
 
 # Create logs directory if it doesn't exist
@@ -51,7 +50,7 @@ echo ""
 # ============================================================================
 
 # python scripts/experiment1.py --num-workers $SLURM_CPUS_PER_TASK "$@"
-python scripts/experiment1.py --num-workers $SLURM_CPUS_PER_TASK --max-shots 10_000_000 --batch-size 10_000
+python scripts/experiment1.py --num-workers 16 --max-shots 10_000_000 --batch-size 10_000
 
 # ============================================================================
 # Job finished
