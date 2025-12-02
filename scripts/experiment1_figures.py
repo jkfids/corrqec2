8baa762961597ed33dfbabadd0f26d3e964543d0
jkfids <jfidelkam@gmail.com>
2025-12-02 17:01:32 +0800

    Write script to generate experiment 1 figures and update paper

---
+++ scripts/experiment1_figures.py
@@ -0,0 +1,204 @@
+import numpy as np
+from scipy.stats import beta
+import matplotlib.pyplot as plt
+import sinter
+
+
+def calc_xi(a, b):
+    return -1 / np.log(1 - a - b)
+
+
+def xi_to_Delta(xi):
+    return 1 - np.exp(-1 / xi)
+
+
+def xi_to_Delta_scaled(xi):
+    return 10 * xi_to_Delta(xi)
+
+
+def Delta_scaled_to_xi(Delta_scaled):
+    return Delta_to_xi(Delta_scaled / 10)
+
+
+def Delta_to_xi(Delta):
+    if isinstance(Delta, np.ndarray):
+        out = np.empty_like(Delta)
+        for i in range(len(Delta)):
+            if Delta[i] == 0:
+                out[i] = np.inf
+            else:
+                out[i] = -1 / np.log(1 - Delta[i])
+    else:
+        if Delta == 0:
+            out = np.inf
+        else:
+            out = -1 / np.log(1 - Delta)
+    return out
+
+
+def distance_to_qubits(distance):
+    return 2 * distance**2 - 1
+
+
+def qubits_to_distance(qubits):
+    return np.round(np.sqrt((qubits + 1) / 2)).astype(int)
+
+
+def distance_to_data_qubits(distance):
+    return distance * distance
+
+
+def data_qubits_to_distance(qubits):
+    return np.round(np.sqrt(qubits)).astype(int)
+
+
+def calc_per_round(per_shot: float, rounds: int):
+    return 0.5 * (1 - (1 - 2 * per_shot) ** (1 / rounds))
+
+
+def binomial_interval(failures, shots, level=0.95):
+    alpha = 0.5
+    a_post = failures + alpha
+    b_post = shots - failures + alpha
+    center = a_post / (a_post + b_post)
+    lower = beta.ppf((1 - level) / 2, a_post, b_post)
+    upper = beta.ppf(1 - (1 - level) / 2, a_post, b_post)
+    return np.array([center, lower, upper])
+
+
+def binomial_interval_per_round(failures, shots, rounds, level=0.95):
+    per_shot_interval = binomial_interval(failures, shots, level)
+    per_round_interval = calc_per_round(per_shot_interval, rounds)
+    return per_round_interval
+
+
+if __name__ == "__main__":
+    # Configure matplotlib
+    plt.rcParams.update(
+        {
+            "font.size": 8,
+            "axes.labelsize": 8,
+            "xtick.labelsize": 7,
+            "ytick.labelsize": 7,
+            "legend.fontsize": 7,
+        }
+    )
+
+    # Load data from CSV
+    tasks = sinter.stats_from_csv_files(
+        "/home/fidel/Projects/corrqec2/data/experiment1_results.csv"
+    )
+
+    # Prepare data for plotting
+    distances = sorted(
+        set(task.json_metadata["experiment_args"]["distance"] for task in tasks)
+    )
+    xis = sorted(
+        set(
+            int(
+                np.round(
+                    calc_xi(
+                        task.json_metadata["noise_model_args"]["model_params"]["a"],
+                        task.json_metadata["noise_model_args"]["model_params"]["b"],
+                    ),
+                    1,
+                )
+            )
+            for task in tasks
+        )
+    )
+
+    results_dict = {distance: {xi: None for xi in xis} for distance in distances}
+
+    for task in tasks:
+        distance = task.json_metadata["experiment_args"]["distance"]
+        model_params = task.json_metadata["noise_model_args"]["model_params"]
+        rounds_mult = int(task.json_metadata["experiment_args"]["rounds"][0])
+        n_rounds = distance * rounds_mult
+        a, b = model_params["a"], model_params["b"]
+        xi = int(np.round(calc_xi(a, b)))
+        shots = task.shots
+        errors = task.errors
+        per_round_interval = binomial_interval_per_round(errors, shots, n_rounds)
+        results_dict[distance][xi] = per_round_interval
+
+    xi_sorted = sorted(results_dict[distance].keys())
+    distance_sorted = sorted(results_dict.keys())
+
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(6.8, 3), sharey=True)
+    colors = plt.rcParams["axes.prop_cycle"].by_key()["color"]
+
+    for i, distance in enumerate(distances):
+        center = [results_dict[distance][xi][0] for xi in xi_sorted]
+        lower = [results_dict[distance][xi][1] for xi in xi_sorted]
+        upper = [results_dict[distance][xi][2] for xi in xi_sorted]
+        ax1.plot(
+            xi_sorted,
+            center,
+            marker=".",
+            label=f"$d={distance}$",
+            linestyle="-",
+            linewidth=1,
+            color=colors[i],
+        )
+        ax1.fill_between(
+            xi_sorted, lower, upper, alpha=0.3, linewidth=0, color=colors[i]
+        )
+    ax1.set_ylabel("Logical error rate per round, $p_{L}$")
+    ax1.set_xlabel("Correlation length, $\\xi$")
+    ax1.legend(loc="lower right")
+    ax1.set_xlim(0.5, 30)
+
+    secax1 = ax1.secondary_xaxis("top", functions=(xi_to_Delta, Delta_to_xi))
+    secax1.set_xlabel("Spectral gap, $\\Delta$")
+    delta_ticks = [0.4, 0.2, 0.1, 0.06, 0.04]
+    secax1.set_xticks(delta_ticks)
+    secax1.tick_params(direction="in")
+
+    for i, xi in enumerate(xis):
+        center = [results_dict[distance][xi][0] for distance in distance_sorted]
+        lower = [results_dict[distance][xi][1] for distance in distance_sorted]
+        upper = [results_dict[distance][xi][2] for distance in distance_sorted]
+        ax2.errorbar(
+            distance_sorted,
+            center,
+            yerr=[
+                np.array(center) - np.array(lower),
+                np.array(upper) - np.array(center),
+            ],
+            fmt=".",
+            color=colors[i],
+            label=f"$\\xi={xi}$",
+        )
+
+    ax2.legend(loc="lower left")
+    ax2.set_xlabel("Code distance, $d$")
+    ax2.set_xlim(4, 20)
+    ax2.set_xticks([5, 10, 15, 19])
+
+    secax2 = ax2.secondary_xaxis(
+        "top", functions=(distance_to_qubits, qubits_to_distance)
+    )
+    secax2.set_xlabel("Total qubits")
+    qubit_ticks = [50, 100, 200, 400, 750]
+    secax2.set_xticks(qubit_ticks)
+    secax2.tick_params(direction="in")
+
+    ax1.text(-0.155, 1.14, "(a)", transform=ax1.transAxes, va="top", ha="left", size=9)
+    ax2.text(-0.075, 1.14, "(b)", transform=ax2.transAxes, va="top", ha="left", size=9)
+
+    for ax in (ax1, ax2):
+        ax.grid(axis="y", alpha=0.5)
+        ax.semilogy()
+        # ax.spines["top"].set_visible(False)
+        # ax.spines["right"].set_visible(False)
+        ax.tick_params(direction="in")
+
+    fig.tight_layout()
+    fig.subplots_adjust(wspace=0.1)
+    fig.savefig(
+        "./project/paper/figures/experiment1.pdf",
+        dpi=600,
+        bbox_inches="tight",
+        pad_inches=0.00,
+    )
