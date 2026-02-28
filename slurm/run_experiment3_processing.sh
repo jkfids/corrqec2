0c7a9bd2e4a43de9269c055e98282600c8616acf
jkfids <jfidelkam@gmail.com>
2026-03-01 00:41:33 +0800

    Update experiment 3 processing memory

--- slurm/run_experiment3_processing.sh
+++ slurm/run_experiment3_processing.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=2
-#SBATCH --mem=8G
+#SBATCH --mem=64G
 #SBATCH --time=0-01:00:00
 #SBATCH --output=/home/jkam/mx95_scratch2/jkam/corrqec2_results/logs/experiment3_processing_%j.out
 #SBATCH --error=/home/jkam/mx95_scratch2/jkam/corrqec2_results/logs/experiment3_processing_%j.err
